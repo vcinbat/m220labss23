c69a58acc2630c0b241ad646db56f0966ecb09cb
vcinbat <122567462+vcinbat@users.noreply.github.com>
2023-01-12 15:59:13 -0500

    Basic Structure of HTML
    Created a basic html structure page and created a basic draw function

---
+++ labs/lab0/js/app.js
@@ -0,0 +1,11 @@
+function setup() {
+    createCanvas(800,600);
+    background(44, 184, 105);
+
+    fill (76,19,102);
+    rect(40,50,100,20);
+}
+
+function draw() {
+    circle(mouseX, mouseY, 17);
+}
